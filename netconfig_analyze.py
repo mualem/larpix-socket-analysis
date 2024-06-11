fc47eb5e8c5a5a39ed2c114f275a8bb40d4881f5
Leon Mualem <mualem@caltech.edu>
2024-06-11 14:52:04 -0700

    Analysis/Selection code for v2d production asic testing

---
+++ netconfig_analyze.py
@@ -0,0 +1,59 @@
+import pandas as pd
+import h5py
+import plotly
+import plotly.express as px
+import chart_studio
+import chart_studio.plotly as py
+import plotly.graph_objs as go
+import csv
+import numpy as np
+from collections import Counter
+from tkinter import filedialog as fd
+
+NumASICchannels = 64
+ShowPlots = False
+
+
+def selectFile(defaultFile):
+	filetypes = (('csv files','*.csv'),('text files','*.txt'),('All files','*.*'))
+	filename = fd.askopenfilename(
+		title='Serial Number file name',
+		initialdir='./data/',
+		initialfile=defaultFile,
+		filetypes=filetypes,
+		multiple=True)
+	return filename
+
+inputCSVfiles=selectFile('')
+listlen=len(inputCSVfiles)
+print('listlen=',listlen)
+print('inputCSVfiles=',inputCSVfiles)
+if listlen<1:
+	exit("Must enter one or more (with ctrl-click) files to use")
+
+netconfigFrame = pd.read_csv(inputCSVfiles[0])
+
+if listlen>1:
+	for filename in inputCSVfiles[1:]:
+		#print(filename)
+		netconfigFrame = netconfigFrame.append(pd.read_csv(filename),ignore_index=True)
+
+print(netconfigFrame)
+badchip = netconfigFrame[ (netconfigFrame['init_chip_results']!=64) ]
+goodchip = netconfigFrame[ (netconfigFrame['init_chip_results']==0) ]
+
+print(badchip)
+
+badchipSN=badchip['ChipSN']
+goodchipSN=goodchip['ChipSN']
+
+BadSNList=[]
+
+for SN in badchipSN.drop_duplicates().to_list() :
+	if SN not in goodchipSN.drop_duplicates().to_list() :
+		print(SN)
+		BadSNList.append(SN)
+
+print(BadSNList)
+
+
